42c9139d97200cd55b48b110389505e67b4fa414
rbiz4 <rbis1001@gmail.com>
2022-09-17 14:54:50 -0400

    Cut out middle variable in mag read

--- magnetometer.cpp
+++ magnetometer.cpp
@@ -17,13 +17,9 @@ void Magnetometer::read (int16_t* outputs) {
 	i2c_write_blocking(i2c, MAG_DEVICE_ADDRESS, &MAG_OUT_X_L, 1, true);
 	i2c_read_blocking(i2c, MAG_DEVICE_ADDRESS, data.data(), 6, false);
 
-	std::int16_t x_current_reading = static_cast<std::int16_t>(static_cast<std::uint16_t>(data[1]) << 8 | static_cast<std::uint16_t>(data[0]));
-	std::int16_t y_current_reading = static_cast<std::int16_t>(static_cast<std::uint16_t>(data[3]) << 8 | static_cast<std::uint16_t>(data[2]));
-	std::int16_t z_current_reading = static_cast<std::int16_t>(static_cast<std::uint16_t>(data[5]) << 8 | static_cast<std::uint16_t>(data[4]));
-
-	outputs[0] = x_current_reading;
-	outputs[1] = y_current_reading;
-	outputs[2] = z_current_reading;
+	outputs[0] = static_cast<std::int16_t>(static_cast<std::uint16_t>(data[1]) << 8 | static_cast<std::uint16_t>(data[0]));
+	outputs[1] = static_cast<std::int16_t>(static_cast<std::uint16_t>(data[3]) << 8 | static_cast<std::uint16_t>(data[2]));
+	outputs[2] = static_cast<std::int16_t>(static_cast<std::uint16_t>(data[5]) << 8 | static_cast<std::uint16_t>(data[4]));
 }
 
 void Magnetometer::init () {
